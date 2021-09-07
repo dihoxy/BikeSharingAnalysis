1424a389768d260a9cdd4b3818318c6b2fd00dac
gtair31 <gtair31@gmail.com>
2021-09-07 08:20:09 -0500

    09/07/2021

--- bikeSharingSQL.sql
+++ bikeSharingSQL.sql
@@ -1,6 +1,21 @@
 CREATE TABLE public.bike_trip_data (
-	ride_id character(16) PRIMARY KEY, rideable_type varchar(15), started_at timestamp, ended_at timestamp,
-	start_station_name varchar(75), start_station_id smallint, end_station_name varchar(75), end_station_id smallint,
-	start_lat decimal(3, 7), start_lng decimal(3, 7), end_lat decimal(3, 7) end_lng decimal(3,7), member_casual character(6)
+	ride_id text PRIMARY KEY, rideable_type text, started_at timestamp, ended_at timestamp,
+	start_station_name text, start_station_id text, end_station_name text, end_station_id text,
+	start_lat decimal(20, 7), start_lng decimal(20, 7), end_lat decimal(20, 7), end_lng decimal(20,7),
+	member_casual text
 
 );
+
+-- Since we know that we will likely calling on the start and end ids
+-- a lot, we will create an index on them
+CREATE INDEX CONCURRENTLY start_station_id_idx
+	ON public.bike_trip_data USING btree
+	(start_station_id ASC NULLS LAST)
+;
+-- CONCURRENTLY is specified since our table already has data as of
+-- this index
+
+CREATE INDEX CONCURRENTLY end_station_id_idx
+	ON public.bike_trip_data USING btree
+	(end_station_id ASC NULLS LAST)
+;
--- changelog.md
+++ changelog.md
@@ -8,3 +8,12 @@
 - Created README and CHANGELOG files 
 - Set version control system (VCS) to Git and created a repository in GitHub
 - Connected VCS to Pycharm
+- Uploaded 04-2020 and 05-2020 to database
+- Corrected date of ride id 97CC2520F1C7A269 from 0006-05-25 to 2020-05-25 for the start_at column
+----
+***2021-09-07***
+- Uploaded 06-2020, 07-2020, 08-2020
+- Deleted all records from database due to errors while importing csvs (*Conversion error due to smallint datatype too small to handle start and end ids)*
+- Changed field "ride_id" to varchar(20), and set start and end ids to type *integer*
+- Changed Pycharm import settings to "first row is header"
+- Starting in 12-2020, start and end ids contain a string of numbers and characters. ***Changed all varchar types to type "text"***
