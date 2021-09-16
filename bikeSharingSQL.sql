44b85783636c9e7fa9f08354f30339fff5a80957
dihoxy <78871260+dihoxy@users.noreply.github.com>
2021-09-16 14:12:07 -0500

    Update bikeSharingSQL.sql

--- bikeSharingSQL.sql
+++ bikeSharingSQL.sql
@@ -1,22 +1,28 @@
 
 -- DDL --
-
 CREATE TABLE public.bike_trip_data (
-	ride_id text PRIMARY KEY, rideable_type text, started_at timestamp, ended_at timestamp,
+	ride_id text PRIMARY KEY, rideable_type text,  started_at timestamp, ended_at timestamp,
 	start_station_name text, start_station_id text, end_station_name text, end_station_id text,
 	start_lat decimal(20, 7), start_lng decimal(20, 7), end_lat decimal(20, 7), end_lng decimal(20,7),
 	member_casual text
 
 );
--- Create a table for 'stations'
-CREATE TABLE stations
-AS (
-    SELECT DISTINCT start_station_id, start_station_name, start_lat, start_lng
-FROM bike_trip_data)
+-- Calculate the interval between start and stops and set it as the value for the 'trip_dur' field
+UPDATE public.bike_trip_data
+SET trip_dur = age(ended_at, started_at)
+;
+
+-- Weekday
+ALTER TABLE public.bike_trip_data
+    DROP COLUMN weekday;
+ALTER TABLE public.bike_trip_data
+    ADD COLUMN weekday int;
+UPDATE bike_trip_data
+    SET weekday = EXTRACT(dow FROM started_at)
 ;
 
 
-
+-- Create a table for 'stations'
 -- Since we are likely to use the start and end stations as filters for a lot
 -- of our queries, we will create an index to improve performance
 CREATE INDEX start_station_name_idx
@@ -47,6 +53,7 @@ SELECT count(*) AS num_null_value
 FROM public.bike_trip_data
 WHERE bike_trip_data.[field_name] IS NULL;
 
+
 -- Querying the member status of electric_bike riders and seeing if there is any trend with null start ids
 SELECT rideable_type, (
     SELECT count(*)
@@ -60,3 +67,16 @@ SELECT rideable_type, (
 FROM bike_trip_data
 WHERE rideable_type = 'electric_bike' AND start_station_id IS NULL
 GROUP BY rideable_type
+
+
+-- DML --
+
+-- Deleted all rows where start_id is null
+DELETE
+FROM bike_trip_data
+WHERE start_station_id IS NULL;
+
+-- Updated 1 missing row where start_station_name was null
+UPDATE bike_trip_data
+SET start_station_name = 'Wood St & Milwaukee Ave'
+WHERE ride_id = '176105D1F8A1216B';
